26897088241572a4c06ccf1b3525752f4ae3fec2
wdhub <yawend@kth.se>
2022-04-04 11:10:17 +0200

    1

--- src/presenters/weatherPresenter.vue
+++ src/presenters/weatherPresenter.vue
@@ -3,6 +3,8 @@
       <vue-weather
         api-key="7c2818a738ef6f68536c2bd139eaa7b0"
         units="uk"
+        bar-color="#7ab8cc"
+        text-color="#456177"
         :latitude="this.$store.state.currentTour.startingPoint.lat.toString()"
         :longitude="this.$store.state.currentTour.startingPoint.lon.toString()"
       />
