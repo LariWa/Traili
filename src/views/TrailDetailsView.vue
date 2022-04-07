244a9bd307df4599523e05660400ca13f0d8d236
Larissa <larissa.wagnerberger@gmail.com>
2022-04-07 13:39:43 +0200

    add title

--- src/views/TrailDetailsView.vue
+++ src/views/TrailDetailsView.vue
@@ -1,18 +1,25 @@
 <template>
-
   <div>
-    <v-btn class="ml-2 mb-10" small @click="backToSearchACB"> Go Back to Search </v-btn>
+    <v-btn class="ml-2 mb-10" small @click="backToSearchACB">
+      Go Back to Search
+    </v-btn>
+
     <v-container v-if="trailInfo">
+      <h1 class="mb-5">{{ trailInfo.title }}</h1>
       <v-row class="white" wrap>
         <v-col cols="6" md="3">
           <v-card class="white" flat>
-            <v-img style="margin: auto"
-                   max-height="60px"
-                   max-width="60px"
-                   :src="iconOne"></v-img>
-            <v-card-text class="text-center font-weight-medium"
-                         style="color: #2196f3"
-                         v-if="trailInfo.time">
+            <v-img
+              style="margin: auto"
+              max-height="60px"
+              max-width="60px"
+              :src="iconOne"
+            ></v-img>
+            <v-card-text
+              class="text-center font-weight-medium"
+              style="color: #2196f3"
+              v-if="trailInfo.time"
+            >
               <div>Time: {{ convertTime(trailInfo.time.min) }} h</div>
             </v-card-text>
           </v-card>
@@ -20,13 +27,17 @@
 
         <v-col cols="6" md="3">
           <v-card class="white" flat>
-            <v-img style="margin: auto"
-                   max-height="60px"
-                   max-width="60px"
-                   :src="iconTwo"></v-img>
-            <v-card-text class="text-center font-weight-medium"
-                         style="color: #2196f3"
-                         v-if="trailInfo.length">
+            <v-img
+              style="margin: auto"
+              max-height="60px"
+              max-width="60px"
+              :src="iconTwo"
+            ></v-img>
+            <v-card-text
+              class="text-center font-weight-medium"
+              style="color: #2196f3"
+              v-if="trailInfo.length"
+            >
               <div>Length: {{ convertDistance(trailInfo.length) }} km</div>
             </v-card-text>
           </v-card>
@@ -34,13 +45,17 @@
 
         <v-col cols="6" md="3">
           <v-card class="white" flat>
-            <v-img style="margin: auto"
-                   max-height="60px"
-                   max-width="60px"
-                   :src="iconThree"></v-img>
-            <v-card-text class="text-center font-weight-medium"
-                         style="color: #2196f3"
-                         v-if="trailInfo.elevation">
+            <v-img
+              style="margin: auto"
+              max-height="60px"
+              max-width="60px"
+              :src="iconThree"
+            ></v-img>
+            <v-card-text
+              class="text-center font-weight-medium"
+              style="color: #2196f3"
+              v-if="trailInfo.elevation"
+            >
               <div>Ascent: {{ trailInfo.elevation.ascent }} m</div>
             </v-card-text>
           </v-card>
@@ -48,13 +63,17 @@
 
         <v-col cols="6" md="3">
           <v-card class="white" flat>
-            <v-img style="margin: auto"
-                   max-height="60px"
-                   max-width="60px"
-                   :src="iconThree"></v-img>
-            <v-card-text class="text-center font-weight-medium"
-                         style="color: #2196f3"
-                         v-if="trailInfo.elevation">
+            <v-img
+              style="margin: auto"
+              max-height="60px"
+              max-width="60px"
+              :src="iconThree"
+            ></v-img>
+            <v-card-text
+              class="text-center font-weight-medium"
+              style="color: #2196f3"
+              v-if="trailInfo.elevation"
+            >
               <div>Descent: {{ trailInfo.elevation.descent }} m</div>
             </v-card-text>
           </v-card>
@@ -84,12 +103,14 @@
               </div>
               <div>
                 Quality of experience:
-                <star-rating :inline="true"
-                             :star-size="20"
-                             :read-only="true"
-                             :show-rating="false"
-                             v-bind:max-rating="6"
-                             v-bind:rating="trailInfo.rating.qualityOfExperience">
+                <star-rating
+                  :inline="true"
+                  :star-size="20"
+                  :read-only="true"
+                  :show-rating="false"
+                  v-bind:max-rating="6"
+                  v-bind:rating="trailInfo.rating.qualityOfExperience"
+                >
                 </star-rating>
                 <!--If you want to pass all the properties of an object as props, you can use v-bind without an argument-->
               </div>
@@ -109,75 +130,74 @@
         <v-col cols="12" md="6">
           <v-card>
             <v-carousel height="300px" v-if="trailInfo.images">
-              <v-carousel-item v-for="(item, i) in trailInfo.images.image"
-                               :key="i"
-                               :src="'http://img.oastatic.com/img/' + item.id + '/.jpg'"
-                               reverse-transition="fade-transition"
-                               transition="fade-transition"></v-carousel-item>
+              <v-carousel-item
+                v-for="(item, i) in trailInfo.images.image"
+                :key="i"
+                :src="'http://img.oastatic.com/img/' + item.id + '/.jpg'"
+                reverse-transition="fade-transition"
+                transition="fade-transition"
+              ></v-carousel-item>
             </v-carousel>
           </v-card>
         </v-col>
       </v-row>
     </v-container>
   </div>
-
 </template>
 
 <!--:src="item.src"-->
 
 <script>
+//import trailData from "./TourDetailsExample.json";
+import VueWeather from "../presenters/weatherPresenter.vue";
+import StarRating from "vue-star-rating";
+import { convertDistance, convertTime, getDifficulty } from "../utilities";
 
-  //import trailData from "./TourDetailsExample.json";
-  import VueWeather from '../presenters/weatherPresenter.vue'
-  import StarRating from 'vue-star-rating'
-  import { convertDistance, convertTime, getDifficulty } from '../utilities'
+export default {
+  data() {
+    return {
+      //trailInfo: trailData.tour[0],
+      //trailInfo: this.$store.state.currentTour,
+      iconOne: "https://www.iconpacks.net/icons/1/free-time-icon-968-thumb.png",
+      iconTwo: "https://static.thenounproject.com/png/2325457-200.png",
+      iconThree: "https://static.thenounproject.com/png/209086-200.png",
+    };
+  },
+  components: {
+    VueWeather,
+    StarRating,
+  },
 
-  export default {
-    data() {
-      return {
-        //trailInfo: trailData.tour[0],
-        //trailInfo: this.$store.state.currentTour,
-        iconOne: 'https://www.iconpacks.net/icons/1/free-time-icon-968-thumb.png',
-        iconTwo: 'https://static.thenounproject.com/png/2325457-200.png',
-        iconThree: 'https://static.thenounproject.com/png/209086-200.png'
-      }
+  props: ["trail"],
+  computed: {
+    trailInfo() {
+      return this.$store.getters.getCurrentTour;
     },
-    components: {
-      VueWeather,
-      StarRating
+  },
+
+  methods: {
+    convertDistance,
+    convertTime,
+    getDifficulty,
+
+    backToSearchACB: function () {
+      this.$emit("returnToSearch");
+    },
+    addToFavACB: function () {
+      this.$emit("addToFav", this.trailInfo);
     },
 
-    props: ['trail'],
-    computed: {
-      trailInfo() {
-        return this.$store.getters.getCurrentTour
-      }
-    },
-
-    methods: {
-      convertDistance,
-      convertTime,
-      getDifficulty,
-
-      backToSearchACB: function() {
-        this.$emit('returnToSearch')
-      },
-      addToFavACB: function() {
-        this.$emit('addToFav', this.trailInfo)
-      },
-
-      /*strippedContent: function(string) {
+    /*strippedContent: function(string) {
                 return string.replace(/<\/?[^>]+(>|$)/g, "");
             },*/
 
-      removeHTML: function(str) {
-        var tmp = document.createElement('DIV')
-        tmp.innerHTML = str
-        return tmp.textContent || tmp.innerText || ''
-      }
-    }
-  }
-
+    removeHTML: function (str) {
+      var tmp = document.createElement("DIV");
+      tmp.innerHTML = str;
+      return tmp.textContent || tmp.innerText || "";
+    },
+  },
+};
 </script>
 
 <!--<style scoped lang="scss">
