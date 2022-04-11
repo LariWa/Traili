80ca2e3aa3d0d5c97d2528f7dc6d356d7d2d4cbd
Larissa <larissa.wagnerberger@gmail.com>
2022-04-11 11:08:17 +0200

    refactored SearchResults

---
+++ src/components/TrailOverviewCard.vue
@@ -0,0 +1,81 @@
+<template>
+  <v-card elevation="2" @click="setCurrentTour(details.id)">
+    <v-img
+      :src="
+        details.primaryImage
+          ? 'http://img.oastatic.com/img/' + details.primaryImage.id + '/.jpg'
+          : 'https://picsum.photos/id/600/1000/300?grayscale'
+      "
+      lazy-src="https://picsum.photos/id/11/100/60"
+      height="200px"
+    >
+      <template v-slot:placeholder>
+        <v-row class="fill-height ma-0" align="center" justify="center">
+          <v-progress-circular
+            indeterminate
+            color="grey lighten-5"
+          ></v-progress-circular>
+        </v-row>
+      </template>
+    </v-img>
+    <v-card-title>
+      {{ details.title }}
+    </v-card-title>
+    <v-card-subtitle v-html="details.shortText">
+      <!-- {{ details.shortText }} -->
+    </v-card-subtitle>
+    <v-card-text class="d-flex flex-wrap">
+      Rating:
+      <v-rating
+        :value="details.rating.qualityOfExperience"
+        color="amber"
+        dense
+        half-increments
+        readonly
+        small
+      ></v-rating>
+      <p class="ml-3 mr-3">-</p>
+      <p>Time: {{ convertTime(details.time.min) }} h</p>
+      <p class="ml-3 mr-3">-</p>
+      <p>Length: {{ convertDistance(details.length) }} km</p>
+      <p class="ml-3 mr-3">-</p>
+      <v-chip
+        :color="
+          details.rating.difficulty == 1
+            ? 'green'
+            : details.rating.difficulty == 2
+            ? 'yellow darken-2'
+            : details.rating.difficulty == 3
+            ? 'red'
+            : 'grey darken-1'
+        "
+        dark
+        small
+      >
+        {{ getDifficulty(details.rating.difficulty) }}
+      </v-chip>
+    </v-card-text>
+    <v-card-actions class="mb-8">
+      <v-btn text color="blue darken-2"> Show More </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+<script>
+import { convertDistance, convertTime, getDifficulty } from "../utilities";
+export default {
+  name: "TrailOverviewCard",
+  props: {
+    details: Object,
+  },
+  methods: {
+    convertDistance,
+    convertTime,
+    getDifficulty,
+
+    setCurrentTour: function (id) {
+      console.log("card");
+      this.$emit("setCurrent", id);
+    },
+  },
+};
+</script>
--- src/views/SearchResultsView.vue
+++ src/views/SearchResultsView.vue
@@ -6,94 +6,29 @@
     <div v-if="results">
       <h1 v-if="Object.keys(results).length > 0">Results</h1>
       <p v-if="details == null">loading...</p>
-      <div v-for="(result, idx) in details" :key="idx">
-        <v-card elevation="2" @click="setCurrentTour(result.id)">
-          <v-img
-            :src="
-              result.primaryImage
-                ? 'http://img.oastatic.com/img/' +
-                  result.primaryImage.id +
-                  '/.jpg'
-                : 'https://picsum.photos/id/600/1000/300?grayscale'
-            "
-            lazy-src="https://picsum.photos/id/11/100/60"
-            height="200px"
-          >
-            <template v-slot:placeholder>
-              <v-row class="fill-height ma-0" align="center" justify="center">
-                <v-progress-circular
-                  indeterminate
-                  color="grey lighten-5"
-                ></v-progress-circular>
-              </v-row>
-            </template>
-          </v-img>
-          <v-card-title>
-            {{ result.title }}
-          </v-card-title>
-          <v-card-subtitle v-html="result.shortText">
-            <!-- {{ result.shortText }} -->
-          </v-card-subtitle>
-          <v-card-text class="d-flex flex-wrap">
-            Rating:
-            <v-rating
-              :value="result.rating.qualityOfExperience"
-              color="amber"
-              dense
-              half-increments
-              readonly
-              small
-            ></v-rating>
-            <p class="ml-3 mr-3">-</p>
-            <p>Time: {{ convertTime(result.time.min) }} h</p>
-            <p class="ml-3 mr-3">-</p>
-            <p>Length: {{ convertDistance(result.length) }} km</p>
-            <p class="ml-3 mr-3">-</p>
-            <v-chip
-              :color="
-                result.rating.difficulty == 1
-                  ? 'green'
-                  : result.rating.difficulty == 2
-                  ? 'yellow darken-2'
-                  : result.rating.difficulty == 3
-                  ? 'red'
-                  : 'grey darken-1'
-              "
-              dark
-              small
-            >
-              {{ getDifficulty(result.rating.difficulty) }}
-            </v-chip>
-          </v-card-text>
-          <v-card-actions class="mb-8">
-            <v-btn
-              text
-              color="blue darken-2"
-              @click="setCurrentTour(result.id)"
-            >
-              Show More
-            </v-btn>
-          </v-card-actions>
-        </v-card>
+      <div v-for="(detail, idx) in details" :key="idx">
+        <TrailOverviewCard
+          :details="detail"
+          @setCurrent="setCurrentTour"
+        ></TrailOverviewCard>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import { convertDistance, convertTime, getDifficulty } from "../utilities";
+import TrailOverviewCard from "../components/TrailOverviewCard.vue";
+
 export default {
   name: "SearchResultsView",
+  components: { TrailOverviewCard },
   props: {
     results: Array,
     details: Array,
   },
   methods: {
-    convertDistance,
-    convertTime,
-    getDifficulty,
-
     setCurrentTour: function (id) {
+      console.log("view");
       this.$emit("setCurrent", id);
     },
   },
