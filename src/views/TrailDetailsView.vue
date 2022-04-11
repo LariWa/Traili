eaf9657e0c7ac0b58e4ad3c0ec7171f625adefcc
Andrea Maderthaner <andmad@kth.se>
2022-04-12 00:00:31 +0200

    Loading within v-carousel

--- src/views/TrailDetailsView.vue
+++ src/views/TrailDetailsView.vue
@@ -1,20 +1,17 @@
 <template>
+
   <div>
     <v-container v-if="trailInfo">
       <v-row class="white" wrap>
         <v-col cols="6" md="3">
           <v-card class="white" flat>
-            <v-img
-              style="margin: auto"
-              max-height="60px"
-              max-width="60px"
-              :src="iconOne"
-            ></v-img>
-            <v-card-text
-              class="text-center font-weight-medium"
-              style="color: #2196f3"
-              v-if="trailInfo.time"
-            >
+            <v-img style="margin: auto"
+                   max-height="60px"
+                   max-width="60px"
+                   :src="iconOne"></v-img>
+            <v-card-text class="text-center font-weight-medium"
+                         style="color: #2196f3"
+                         v-if="trailInfo.time">
               <div>Time: {{ convertTime(trailInfo.time.min) }} h</div>
             </v-card-text>
           </v-card>
@@ -22,17 +19,13 @@
 
         <v-col cols="6" md="3">
           <v-card class="white" flat>
-            <v-img
-              style="margin: auto"
-              max-height="60px"
-              max-width="60px"
-              :src="iconTwo"
-            ></v-img>
-            <v-card-text
-              class="text-center font-weight-medium"
-              style="color: #2196f3"
-              v-if="trailInfo.length"
-            >
+            <v-img style="margin: auto"
+                   max-height="60px"
+                   max-width="60px"
+                   :src="iconTwo"></v-img>
+            <v-card-text class="text-center font-weight-medium"
+                         style="color: #2196f3"
+                         v-if="trailInfo.length">
               <div>Length: {{ conversion(trailInfo.length) }} km</div>
             </v-card-text>
           </v-card>
@@ -40,17 +33,13 @@
 
         <v-col cols="6" md="3">
           <v-card class="white" flat>
-            <v-img
-              style="margin: auto"
-              max-height="60px"
-              max-width="60px"
-              :src="iconThree"
-            ></v-img>
-            <v-card-text
-              class="text-center font-weight-medium"
-              style="color: #2196f3"
-              v-if="trailInfo.elevation"
-            >
+            <v-img style="margin: auto"
+                   max-height="60px"
+                   max-width="60px"
+                   :src="iconThree"></v-img>
+            <v-card-text class="text-center font-weight-medium"
+                         style="color: #2196f3"
+                         v-if="trailInfo.elevation">
               <div>Ascent: {{ trailInfo.elevation.ascent }} m</div>
             </v-card-text>
           </v-card>
@@ -58,17 +47,13 @@
 
         <v-col cols="6" md="3">
           <v-card class="white" flat>
-            <v-img
-              style="margin: auto"
-              max-height="60px"
-              max-width="60px"
-              :src="iconThree"
-            ></v-img>
-            <v-card-text
-              class="text-center font-weight-medium"
-              style="color: #2196f3"
-              v-if="trailInfo.elevation"
-            >
+            <v-img style="margin: auto"
+                   max-height="60px"
+                   max-width="60px"
+                   :src="iconThree"></v-img>
+            <v-card-text class="text-center font-weight-medium"
+                         style="color: #2196f3"
+                         v-if="trailInfo.elevation">
               <div>Descent: {{ trailInfo.elevation.descent }} m</div>
             </v-card-text>
           </v-card>
@@ -101,14 +86,12 @@
               </div>
               <div>
                 Quality of experience:
-                <star-rating
-                  :inline="true"
-                  :star-size="20"
-                  :read-only="true"
-                  :show-rating="false"
-                  v-bind:max-rating="6"
-                  v-bind:rating="trailInfo.rating.qualityOfExperience"
-                >
+                <star-rating :inline="true"
+                             :star-size="20"
+                             :read-only="true"
+                             :show-rating="false"
+                             v-bind:max-rating="6"
+                             v-bind:rating="trailInfo.rating.qualityOfExperience">
                 </star-rating>
                 <!--If you want to pass all the properties of an object as props, you can use v-bind without an argument-->
               </div>
@@ -121,22 +104,19 @@
                 <v-icon dark>mdi-heart</v-icon>
               </v-btn>
 
-              <v-snackbar
-                v-model="snackbar"
-                :timeout="timeout"
-              >
+              <v-snackbar v-model="snackbar" :timeout="timeout">
                 {{ text }}
 
                 <template v-slot:action="{ attrs }">
-                  <v-btn
-                    color="blue"
-                    text
-                    v-bind="attrs"
-                    @click="snackbar = false"
-                  >
-                    Close
-                  </v-btn>
-                </template>
+                        <v-btn
+                          color="blue"
+                          text
+                          v-bind="attrs"
+                          @click="snackbar = false"
+                        >
+                          Close
+                        </v-btn>
+                      </template>
               </v-snackbar>
 
               <!--if added color="pink" if taken out grey-->
@@ -155,24 +135,24 @@
         <v-col cols="12" md="6">
           <v-card>
             <v-carousel height="300px" v-if="trailInfo.images">
-              <v-carousel-item
-                v-for="(item, i) in trailInfo.images.image"
-                :key="i"
-                :src="'http://img.oastatic.com/img/' + item.id + '/.jpg'"
-                reverse-transition="fade-transition"
-                transition="fade-transition"
-              >
-              
-               <!-- <template v-slot:placeholder>
-              <v-row class="fill-height ma-0" align="center" justify="center">
-                <v-progress-circular
-                  indeterminate
-                  color="grey lighten-5"
-                ></v-progress-circular>
-              </v-row>
-            </template>-->
-             
-
+              <v-carousel-item v-for="(item, i) in trailInfo.images.image"
+                               :key="i"
+                               >
+                               <!-- insert before '>'
+                               reverse-transition="fade-transition"
+                               transition="fade-transition"
+                               -->
+                <v-img :src="'http://img.oastatic.com/img/' + item.id + '/.jpg'" lazy-src="https://picsum.photos/id/11/100/60">
+                  >
+                  <template v-slot:placeholder>
+                        <v-row class="fill-height ma-0" align="center" justify="center">
+                          <v-progress-circular
+                            indeterminate
+                            color="grey lighten-5"
+                          ></v-progress-circular>
+                        </v-row>
+                      </template>
+                </v-img>
               </v-carousel-item>
             </v-carousel>
           </v-card>
@@ -189,111 +169,114 @@
 
     </v-container>
   </div>
+
 </template>
 
 <script>
-//import trailData from "./TourDetailsExample.json";
-import VueWeather from "../presenters/weatherPresenter.vue";
-import StarRating from "vue-star-rating";
 
-export default {
-  data() {
-    return {
-      //trailInfo: trailData.tour[0],
-      //trailInfo: this.$store.state.currentTour,
-      iconOne: "https://www.iconpacks.net/icons/1/free-time-icon-968-thumb.png",
-      iconTwo: "https://static.thenounproject.com/png/2325457-200.png",
-      iconThree: "https://static.thenounproject.com/png/209086-200.png",
-      snackbar: false,
-      text: '',
-      timeout: 1000,
-    };
-  },
-  components: {
-    VueWeather,
-    StarRating,
-  },
+  //import trailData from "./TourDetailsExample.json";
+  import VueWeather from '../presenters/weatherPresenter.vue'
+  import StarRating from 'vue-star-rating'
 
-  //props: ["trail"],
-  props: {
-    trail: Array,
-  },
-  
-  computed: {
-    trailInfo() {
-      return this.$store.getters.getCurrentTour;
-    },
-    addedToFav() {
-      var found = this.$store.state.favourites.find(element => element.id === this.trailInfo.id);
-      /*state: {
-          favourites: [], //array of objects
-          currentTourID: "",
-          categoriesPromiseState: { data: [] },
-          currentTourPromiseState: { data: [] },
-        }, 
-        in index.js*/
-      
-      if (this.$store.state.favourites.indexOf(found) != -1) {
-        return true;
+  export default {
+    data() {
+      return {
+        //trailInfo: trailData.tour[0],
+        //trailInfo: this.$store.state.currentTour,
+        iconOne: 'https://www.iconpacks.net/icons/1/free-time-icon-968-thumb.png',
+        iconTwo: 'https://static.thenounproject.com/png/2325457-200.png',
+        iconThree: 'https://static.thenounproject.com/png/209086-200.png',
+        snackbar: false,
+        text: '',
+        timeout: 1000
       }
+    },
+    components: {
+      VueWeather,
+      StarRating
+    },
 
-      return false;
+    //props: ["trail"],
+    props: {
+      trail: Array
+    },
+
+    computed: {
+      trailInfo() {
+        return this.$store.getters.getCurrentTour
+      },
+      addedToFav() {
+        var found = this.$store.state.favourites.find(element => element.id === this.trailInfo.id)
+        /*state: {
+                favourites: [], //array of objects
+                currentTourID: "",
+                categoriesPromiseState: { data: [] },
+                currentTourPromiseState: { data: [] },
+              },
+              in index.js*/
+
+        if (this.$store.state.favourites.indexOf(found) != -1) {
+          return true
+        }
+
+        return false
+      }
+    },
+
+    methods: {
+      conversion: function(length) {
+        var formattedNumber = length / 1000
+        return formattedNumber.toFixed(1)
+      },
+      convertTime: function(min) {
+        var hours = min / 60
+        var h = Math.floor(hours)
+        var minutes = (hours - h) * 60
+        var m = Math.floor(minutes)
+
+        return `${this.twoDigits(h)}:${this.twoDigits(m)}`
+      },
+      twoDigits: function(num) {
+        return num.toString().padStart(2, '0')
+      },
+
+      backToSearchACB: function() {
+        this.$emit('returnToSearch')
+      },
+      addToFavACB: function() {
+        this.text = 'Trail added to favourites'
+        this.$emit('addToFav', this.trailInfo)
+      },
+
+      removeFromFavACB: function() {
+        this.text = 'Trail removed from favourites'
+        this.$emit('removeFromFav', this.trailInfo)
+      },
+
+      /*strippedContent: function(string) {
+                  return string.replace(/<\/?[^>]+(>|$)/g, "");
+              },*/
+
+      removeHTML: function(str) {
+        var tmp = document.createElement('DIV')
+        tmp.innerHTML = str
+        return tmp.textContent || tmp.innerText || ''
+      },
+
+      dificulty: function(value) {
+        if (value == 1) {
+          return 'easy'
+        } else if (value == 2) {
+          return 'moderate'
+        } else if (value == 3) {
+          return 'difficult'
+        } else {
+          return 'difficulty unknown'
+        }
+      }
     }
-  },
+  }
 
-  methods: {
-    conversion: function (length) {
-      var formattedNumber = length / 1000;
-      return formattedNumber.toFixed(1);
-    },
-    convertTime: function (min) {
-      var hours = min / 60;
-      var h = Math.floor(hours);
-      var minutes = (hours - h) * 60;
-      var m = Math.floor(minutes);
-
-      return `${this.twoDigits(h)}:${this.twoDigits(m)}`;
-    },
-    twoDigits: function (num) {
-      return num.toString().padStart(2, "0");
-    },
-
-    backToSearchACB: function () {
-      this.$emit("returnToSearch");
-    },
-    addToFavACB: function () {
-      this.text = 'Trail added to favourites';
-      this.$emit("addToFav", this.trailInfo);
-    },
-
-    removeFromFavACB: function() {
-      this.text = 'Trail removed from favourites';
-      this.$emit("removeFromFav", this.trailInfo);
-    },
-
-    /*strippedContent: function(string) {
-            return string.replace(/<\/?[^>]+(>|$)/g, "");
-        },*/
-
-    removeHTML: function (str) {
-      var tmp = document.createElement("DIV");
-      tmp.innerHTML = str;
-      return tmp.textContent || tmp.innerText || "";
-    },
-
-    dificulty: function (value) {
-      if (value == 1) {
-        return "easy";
-      } else if (value == 2) {
-        return "moderate";
-      } else if (value == 3) {
-        return "difficult";
-      } else {
-        return "difficulty unknown";
-      }
-    },
-  },
-};
 </script>
 
 
@@ -301,6 +284,6 @@ export default {
     <div v-for="(element, index) in trailInfo.wayType.elements" :key="index">
         {{ element.title }}
         </div>
-    
+
     {{trailInfo.wayType.elements[0].title}}
     -->
