d0b262cbac62afdaae52d2ec0d810701c6f85379
AndreaMaderthaner <95613373+AndreaMaderthaner@users.noreply.github.com>
2022-05-10 09:23:12 +0200

    Delete APIConfig.js

--- src/APIConfig.js
+++
@@ -1,4 +0,0 @@
-const project = "api-kthroyalinstituteoftechnology";
-const key = "RFPGCUCE-EMWGM3KA-4OSSFEWT";
-const gmapKey = "AIzaSyCcmWjWcUj-Zz-W8NMNAoMAReR1ZILH-dE";
-export { project, key, gmapKey };
